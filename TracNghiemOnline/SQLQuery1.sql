1f38131aee0501a00abc56ebe8cca7b199580415
le quyen <lequyenk41@outlook.com>
2021-11-03 14:06:13 +0700

    Cập nhật folder 2 website
    Cập nhật folder 2 website

--- TracNghiemOnline/SQLQuery1.sql
+++
@@ -1,9 +0,0 @@
-﻿  DBCC CHECKIDENT (tests, RESEED, 0)
-  select unit from questions
- 
-UPDATE questions
-SET unit = N'Lý thuyết chuyên ngành'
-WHERE unit = '3'; 
- 
- update questions
- set active = 1
